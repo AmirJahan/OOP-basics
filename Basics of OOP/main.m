641a0b29870454ceb5bab08e5e551c37215c6600
Amir J <amir.jahan@gmail.com>
2017-10-03 09:22:37 -0700

    Initial Commit

---
+++ Basics of OOP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Basics of OOP
+//
+//  Created by Amir on 10/3/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
